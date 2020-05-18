53e8f0e45e579dfecae9f61a011acecf33a8fd83
Siva Cheerla <sivacheerla@live.com>
2020-05-18 17:10:41 +0530

    18/05/20

--- src/main/java/web/StartWeb.java
+++ src/main/java/web/StartWeb.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //scanBasePackages = {"jpa","web"}
 
-@SpringBootApplication
+@SpringBootApplication()
 public class StartWeb // extends SpringBootServletInitializer
 {
 	public static void main(String... args) {
